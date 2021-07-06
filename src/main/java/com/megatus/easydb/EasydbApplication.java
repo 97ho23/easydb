57fa0f0fc780fd0f129c37b6c10b6f677c8b3912
97ho23 <97ho23@gmail.com>
2021-07-06 13:25:26 +0900

    Initial commit

---
+++ src/main/java/com/megatus/easydb/EasydbApplication.java
@@ -0,0 +1,16 @@
+package com.megatus.easydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasydbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasydbApplication.class, args);
+
+
+
+	}
+
+}
